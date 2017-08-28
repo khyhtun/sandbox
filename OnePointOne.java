c7eab49de9da94a3d6f1f5cdc80de8d4c79d763b
Khy Htun <khyhtun@gmail.com>
2017-08-27 17:17:30 -0700

    Adding initial code for OnePointOne.java

---
+++ OnePointOne.java
@@ -0,0 +1,13 @@
+
+public class OnePointOne
+{
+	public boolean isAllUniqueCharacters(String str)
+	{
+		System.out.println(str.charAt(0));
+		return false;
+	}
+	public static void main(String... args)
+	{
+		new OnePointOne().isAllUniqueCharacters("hello");
+	}
+}
